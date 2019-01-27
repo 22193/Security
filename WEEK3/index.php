04463b204847bcb2fba4706ae86d6a630496232f
Idriss El Mellaoui <22193@ma-web.nl>
2019-01-27 13:35:51 +0100

    Update index.php

--- WEEK3/index.php
+++ WEEK3/index.php
@@ -1,44 +1,27 @@
 <?php
-// SELECT
-//1. Connectie: Demonstration code only
 $dbc = new PDO('mysql:host=localhost;dbname=sec', 'root', 'root');
-//2. Statement (Named placeholders)
 $stmt = $dbc->prepare("SELECT * FROM users WHERE username = :username");
-//$stmt = $dbc->prepare("SELECT * FROM users");
-// 3. Parameter binding
 $stmt->bindParam('username', $username);
-// 4. Waarden in variabelen plaatsen
 $username = 'Unknown';
-// 5. Execute
 $stmt->execute() or die('Error select after PDQ');
-// 6. WHILE-LOOP
 while ($row = $stmt->fetch()) {
     echo $row['userid'] . ' ' . $row['username'] . '<br>';
 }
-// INSERT
-// Prepare (positional placeholder)
 $stmt = $dbc->prepare("INSERT INTO users VALUES (?, ?, ?)");
-// Bind
 $stmt->bindParam(1, $userid);
 $stmt->bindParam(2, $username);
 $stmt->bindParam(3, $password);
-// Waarden
 $userid = 0;
 $username = 'Unknown';
 $password = '123';
-//$stmt->execute() or die ('Error insert after PDO');
-$stmt->execute();
-// Prepare (named placeholder)
+$stmt->execute() or die ('Error insert after PDO');
 $stmt = $dbc->prepare("INSERT INTO users VALUES (:userid, :username, :password)");
-// Bind
 $stmt->bindParam(':userid', $userid);
 $stmt->bindParam(':username', $username);
 $stmt->bindParam('password', $password);
-// Waarden
 $userid = 0;
 $username = 'Named placeholder';
 $password = '123';
-//$stmt->execute() or die ('Error insert after PDO');
 $stmt = null;
 $dbc = null;
 exit();
